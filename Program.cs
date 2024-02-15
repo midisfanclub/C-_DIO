6e73b2c76034e17c71b4c50fdd443e015c82efe6
AlicePlentz <alicecplentz@gmail.com>
2024-02-15 15:52:39 -0300

    Operadores aritiméticos

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿//using C__DIO.Classes;
+﻿using C__DIO.operadores.aritmeticos;
+
+
+Calculadora calc = new Calculadora(); //instaciamos a classe Calculadora
+
+calc.Somar(10, 30);
+calc.Subtrair(13, 50);
+calc.Multiplicar(15, 45);
+calc.Dividir(2, 2);
+calc.Potencia(3, 3);
+calc.Seno(30);
+calc.Tangente(30);
+calc.Coseno(30);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Pessoa p = new Pessoa();
