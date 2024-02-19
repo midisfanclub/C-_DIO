3883ed0594fc259f3ea7ae28dc1cca8231f481d8
AlicePlentz <alicecplentz@gmail.com>
2024-02-19 15:10:20 -0300

    Repetição + menu interativo

--- Program.cs
+++ Program.cs
@@ -1,19 +1,128 @@
 ﻿using System.Collections;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 using C__DIO;
 
-int numero = 5;
-int contador = 1;
 
-while (contador <= 10)
-{
-    Console.WriteLine($"{numero} x {contador} = {numero * contador}");
-    contador++;
 
-    if (contador == 6)
-    {
-        break;
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// string opcao;
+
+// while(true)
+// {
+//     Console.WriteLine("Digite a sua opção:");
+//     Console.WriteLine("1 - Cadastrar Cliente");
+//     Console.WriteLine("2 - Buscar cliente");
+//     Console.WriteLine("3 - Apagar cliente");
+//     Console.WriteLine("4 - Encerrar");
+
+//     opcao = Console.ReadLine();
+
+//     switch(opcao)
+//     {
+//         case "1":
+//             Console.WriteLine("Cadastro de cliente");
+//             break;
+        
+//         case "2":
+//             Console.WriteLine("Busca de cliente:");
+//             break;
+        
+//         case "3":
+//             Console.WriteLine("Apagar cliente");
+//             break;
+        
+//         case "4":
+//             Console.WriteLine("Encerrar");
+//             Environment.Exit(0);
+//             break;    
+
+//         default:
+//         Console.WriteLine("opção inválida");
+//         break;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int soma = 0, numero;
+
+// do
+// {
+//     Console.WriteLine("digite um número (0 para parar): ");
+//     numero = Convert.ToInt32(Console.ReadLine());
+
+//     soma += numero;
+
+// } while(numero != 0);
+
+// Console.WriteLine($"Total = {soma}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int numero = 5;
+// int contador = 1;
+
+// while (contador <= 10)
+// {
+//     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+//     contador++;
+
+//     if (contador == 6)
+//     {
+//         break;
+//     }
+// }
 
 
 
